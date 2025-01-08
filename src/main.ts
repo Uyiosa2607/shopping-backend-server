b826a1bed0f46da2a282cf5879d90caacc5da653
Uyiosa2607 <aghahowaosamudiamen03@gmail.com>
2025-01-08 12:47:15 +0100

    expanded cors origin list

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "https://kultra-devices.vercel.app",
+  origin: ["https://kultra-devices.vercel.app", "http://localhost:3000"],
   credentials: true,
 };
 
