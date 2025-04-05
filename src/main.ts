80c338fcdcd1b0d95fb102f7f1cc76c32cd52919
Uyiosa2607 <aghahowaosamudiamen03@gmail.com>
2025-04-05 22:01:56 +0100

    changes made

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 24 * 60 * 60 * 1000,
-      secure: true,
+      secure: false,
       sameSite: "none",
       httpOnly: false,
     },
