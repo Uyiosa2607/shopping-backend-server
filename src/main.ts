987fcac6421ea7ea2512f530b9d7cf2601b315b8
Uyiosa2607 <aghahowaosamudiamen03@gmail.com>
2025-04-07 13:03:01 +0100

    changes made again to cookies

--- src/main.ts
+++ src/main.ts
@@ -47,7 +47,7 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
       secure: false,
-      sameSite: "lax",
+      sameSite: "none",
       httpOnly: process.env.NODE_ENV === "production",
     },
   })
