c280bd53cdcd0de2a0a203c404e6763bd9ba101a
Uyiosa2607 <aghahowaosamudiamen03@gmail.com>
2025-04-07 12:50:00 +0100

    changes made again

--- src/main.ts
+++ src/main.ts
@@ -48,7 +48,7 @@ app.use(
       maxAge: 1000 * 60 * 60 * 24,
       secure: true,
       sameSite: "none",
-      httpOnly: true,
+      httpOnly: process.env.NODE_ENV === "production",
     },
   })
 );
